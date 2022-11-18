1a98f1b4bca91b6e64c15e7cc1ef257a313ba751
renato areval <renattoareval@gmail.com>
2022-11-18 16:44:18 -0400

    modificando dequeue

--- TestaFIla.java
+++ TestaFIla.java
@@ -1,58 +1,43 @@
 
-
 public class TestaFIla {
-    private  ListaLIgada lista;
- 
-     public TestaFIla(){
-         this.lista = new ListaLIgada();
-     }
- 
-     public void enqueue(String novoValor){
-         this.lista.adiciona(novoValor);
-     }
- 
-     public void dequeue(){
-         this.lista.remover(this.get());
-     }
- 
-     public String get(){ // pega o primeiro elemento para usar no método dequeue
-         return this.lista.getPrimeiro().getValor();
-     }
+    private ListaLIgada lista;
 
-     public int size(){
+    public TestaFIla() {
+        this.lista = new ListaLIgada();
+    }
+
+    public void enqueue(String novoValor) {
+        this.lista.adiciona(novoValor);
+    }
+
+    public void dequeue() {
+        if (size() == 0) {
+            throw new IllegalArgumentException("lista vazia");
+        }
+
+        this.lista.remover(this.get());
+    }
+
+    public String get() { // pega o primeiro elemento para usar no método dequeue
+        return this.lista.getPrimeiro().getValor();
+    }
+
+    public int size() {
         return this.lista.getTamanho();
-     }
+    }
 
-     public boolean isEmpty(){
-        if(lista.getTamanho() == 0){
+    public boolean isEmpty() {
+        if (lista.getTamanho() == 0) {
             return true;
-        }else{
+        } else {
             return false;
         }
-     } 
+    }
 
-     public static void main(String[] args) {
+    public static void main(String[] args) {
         TestaFIla fila = new TestaFIla();
 
-
-        fila.enqueue("1");
-        fila.enqueue("2");
-        fila.enqueue("3");
-        fila.enqueue("4");
-        fila.enqueue("5");
-
-        System.out.println("Primeiro da fila " +fila.get());
         fila.dequeue();
-        System.out.println("Novo primeiro da fila " +fila.get());
-        System.out.println("Tamanho da fila " + fila.size());
-        System.out.println(fila.isEmpty()) ;
+
     }
- }
- 
-
-
-
-
-
-   
-
+}
